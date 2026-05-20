0cd6d77266d967e4046ccf8c2fb6c2f5a2d65163
Yamkela Venfolo <yamkela.venfolo@standardbank.co.za>
2026-05-20 22:48:20 +0200

    update RabbitMQConfig

--- src/main/java/com/fintrack/api/config/RabbitMQConfig.java
+++ src/main/java/com/fintrack/api/config/RabbitMQConfig.java
@@ -1,8 +1,13 @@
 package com.fintrack.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +15,16 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     @Bean
+    public TopicExchange transactionExchange(@Value("${fintrack.rabbitmq.exchange}") String exchange) {
+        return new TopicExchange(exchange, true, false);
+    }
+
+    @Bean
     public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new Jackson2JsonMessageConverter(mapper);
     }
 
     @Bean
